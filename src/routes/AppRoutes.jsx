34776efb4f85bef1fb07eb1c26729ad18582b93e
WebWithAstha <lodhiastha85@gmail.com>
2024-06-29 22:14:15 +0530

    App routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../components/Home';
+import About from '../components/About';
+import NotFound from '../components/NotFound';
+// import GalleryContainer from '../components/GalleryContainer';
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
