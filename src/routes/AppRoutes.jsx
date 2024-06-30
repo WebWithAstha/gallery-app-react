899ebc528408dc57634662d2388340241a2aa73d
WebWithAstha <lodhiastha85@gmail.com>
2024-06-30 22:50:18 +0530

    search route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home';
 import About from '../components/About';
 import NotFound from '../components/NotFound';
+import Search from '../components/Search';
 // import GalleryContainer from '../components/GalleryContainer';
 
 
@@ -11,6 +12,7 @@ const AppRoutes = () => {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search/:query" element={<Search />} />
         <Route path="*" element={<NotFound />} />
     </Routes>
   )
