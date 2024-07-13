306c4a13b52f064aeae8e4d9ce19b988754bbe1e
WebWithAstha <lodhiastha85@gmail.com>
2024-07-13 19:05:59 +0530

    menu added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
 
   return (
-    <div className='w-full pb-20 text-[#333533] min-h-screen relative z-[1] bg-[#333533] xl:bg-[#cfdbd5]'>
+    <div className='w-full pb-20 text-[#333533] overflow-x-hidden min-h-screen relative z-[1] bg-[#333533] xl:bg-[#cfdbd5]'>
       <img className='w-full h-full fixed top-0 left-0 object-cover brightness-[85%] contrast-125' src="https://plus.unsplash.com/premium_photo-1706430433638-b9f3183a496e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
       <div className="flex w-full h-full relative">
         <SideNav />
