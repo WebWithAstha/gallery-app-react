c96ad7d7d201b4233f85165c0793683a7f581a88
WebWithAstha <lodhiastha85@gmail.com>
2024-07-25 22:40:42 +0530

    download options added

--- src/components/SingleImg.jsx
+++ src/components/SingleImg.jsx
@@ -44,7 +44,18 @@ const SingleImg = () => {
                             <div className="xl:w-10 w-8 xl:h-10 h-8 rounded-full bg-yellow-800"></div>
                             <h1 className='p-2 font-bold'>{img.photographer}</h1>
                         </div>
-                        <button onClick={e=>downloadImage(img.src.large2x,`img${img.id}_from_astha_gallery_app`)} className='bg-emerald-500 h-max text-white rounded-lg px-8 py-2'>Download</button>
+
+
+                        <div className="bg-emerald-500 relative z-0 h-max group pointer-events-auto rounded-lg px-4 xl:px-8 py-2">
+            <span  onClick={e => downloadImage(img.src.large2x, `img${img.id}_from_astha_gallery_app`)} className='font-medium capitalize'>Download</span>
+            <div className="opts w-full absolute h-0 overflow-hidden group-hover:h-[12.5rem] transition-all duration-300 top-[110%] border-[#afb0ac] rounded left-0 z-[999] bg-[#e8eddf]">
+              <h2 onClick={e => downloadImage(img.src.large2x, `img${img.id}_360p_from_astha_gallery_app`)} className='w-full pointer-events-auto px-4 py-2 hover:bg-[#c8cebf] text-center border-b border-[#afb0ac]'>360p</h2>
+              <h2 onClick={e => downloadImage(img.src.original, `img${img.id}_720p_from_astha_gallery_app`)} className='w-full pointer-events-auto px-4 py-2 hover:bg-[#c8cebf] text-center border-b border-[#afb0ac]'>720p</h2>
+              <h2 onClick={e => downloadImage(img.src.medium, `img${img.id}_240p_from_astha_gallery_app`)} className='w-full pointer-events-auto px-4 py-2 hover:bg-[#c8cebf] text-center border-b border-[#afb0ac]'>240p</h2>
+              <h2 onClick={e => downloadImage(img.src.small, `img${img.id}_144p_from_astha_gallery_app`)} className='w-full pointer-events-auto px-4 py-2 hover:bg-[#c8cebf] text-center border-b border-[#afb0ac]'>144p</h2>
+              <h2 onClick={e => downloadImage(img.src.tiny, `img${img.id}_200x280_from_astha_gallery_app`)} className='w-full pointer-events-auto px-4 py-2 hover:bg-[#c8cebf] text-center border-b border-[#afb0ac]'>200x280</h2>
+            </div>
+          </div>
                     </div>
                     <div className="img w-full xl:mb-0 mb-10 h-fit mt-4 flex items-center justify-center">
                         <img className="h-full xl:h-[80vh] object-contain" src={img && img.src.large2x} alt="" />
